0c9b5d6daaf5872936e56902ce5154d5e7139678
Faisal Mahmud <faisal@willandskill.se>
2018-10-10 10:43:01 +0200

    Add TextareaAutosize

--- src/sections/newsviewer/components/NewsDetail.js
+++ src/sections/newsviewer/components/NewsDetail.js
@@ -1,15 +1,47 @@
 import React, { Component } from "react";
 
 export default class NewsDetail extends Component {
+  renderImage() {
+    const { selectedItem } = this.props;
+    if (selectedItem.image) {
+      return (
+        <div className="card-body m-0 p-0">
+          <img src={selectedItem.image} className="img-fluid" />
+        </div>
+      );
+    }
+    return null;
+  }
+
+  renderReadMoreBtn() {
+    const { selectedItem } = this.props;
+    if (selectedItem.source_url) {
+      return (
+        <a
+          className="btn btn-info btn-sm"
+          href={selectedItem.source_url}
+          target="_blank"
+        >
+          READ MORE
+        </a>
+      );
+    }
+    return null;
+  }
+
   render() {
     const { selectedItem } = this.props;
     if (selectedItem) {
       return (
         <div className="card">
+          {this.renderImage()}
           <div className="card-header">
-            <p className="lead">{selectedItem.heading}</p>
+            <p className="m-0 lead">{selectedItem.heading}</p>
           </div>
-          <div className="card-body">{selectedItem.summary}</div>
+          <div className="card-body">
+            <p>{selectedItem.summary}</p>
+            {this.renderReadMoreBtn()}
+          </div>
         </div>
       );
     }
@@ -18,3 +50,5 @@ export default class NewsDetail extends Component {
     );
   }
 }
+
+const styles = {};
